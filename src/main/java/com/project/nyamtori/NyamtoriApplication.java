fdd33cfb661491cd17609ee238b8ddc6c7957917
Seoyeon-park0103 <seoyeon-park@naver.com>
2026-01-26 14:50:44 +0900

    초기 세팅

---
+++ src/main/java/com/project/nyamtori/NyamtoriApplication.java
@@ -0,0 +1,13 @@
+package com.project.nyamtori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyamtoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyamtoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/nyamtori/NyamtoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nyamtori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyamtoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
